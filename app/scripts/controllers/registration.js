5bfc93e8a8a458f24a92123ba512cbb6ec867180
Eugene Dobrorodnov <ei.dobrorodnov@gmail.com>
2015-08-04 10:46:25 +0300

    Auth refactoring

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -10,28 +10,23 @@
 angular.module('mailboxFrontendApp')
   .controller('RegistrationCtrl', function ($rootScope, $scope, $http, $cookieStore, Auth) {
 
-    $scope.signup = function(){
-      $http.post('http://localhost:3005/api/v1/auth', {user: $scope.signup_credentials}).
-      success(function(data, status, headers, config){
+    $scope.signUp = function(){
+      Auth.signUp($scope.signup_credentials).
+      success(function(data, status, headers, config) {
         if(data.success){
-          $cookieStore.put('current_user', JSON.stringify(data.user));
-          $rootScope.set_auth_headers(data.user.email, data.user.authentication_token);
-          $rootScope.current_user = data.user;
+          Auth.init_current_user(data.user);
         }else{
           console.log(data)
         }
       }).
       error(function(error){ console.log(error) });
-    }
+    };
 
-    $scope.signin = function(){
-      $http.post('http://localhost:3005/api/v1/auth/sign_in', {user: $scope.signin_credentials}).
-      success(function(data, status, headers, config){
+    $scope.signIn = function(){
+      Auth.signIn($scope.signin_credentials).
+      success(function(data, status, headers, config) {
         if(data.success){
-          console.log(data)
-          $cookieStore.put('current_user', JSON.stringify(data.user));
-          $rootScope.set_auth_headers(data.user.email, data.user.authentication_token);
-          $rootScope.current_user = data.user;
+          Auth.init_current_user(data.user);
         }else{
 
         }
@@ -39,12 +34,10 @@ angular.module('mailboxFrontendApp')
       error(function(error){
         $scope.signin_credentials.errors = error.message
       });
-    }
+    };
 
-    $scope.signout = function(){
-      $cookieStore.remove('current_user');
-      $http.defaults.headers.common['X-User-Email'] = null;
-      $http.defaults.headers.common['X-User-Token'] = null;
-      $rootScope.current_user = null;
-    }
+    $scope.signOut = function() {
+      Auth.signOut();
+    };
+
   });
--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -8,8 +8,30 @@
  * Service in the mailboxFrontendApp.
  */
 angular.module('mailboxFrontendApp')
-  .service('Auth', function ($resource) {
-    return $resource('http://localhost:3005/api/v1/auth', {}, {
-      query: {method:'POST', params:{user: null}, isArray:true}
-    });
+  .factory('Auth', function ($http, $rootScope, $cookieStore) {
+    var resource_url = 'http://localhost:3005/api/v1/auth';
+    var self = this;
+
+    self.signUp = function (user_params) {
+        return $http.post(resource_url, {user: user_params});
+    };
+
+    self.signIn = function (user_params) {
+        return $http.post(resource_url + '/sign_in', {user: user_params});
+    };
+
+    self.signOut = function() {
+      $cookieStore.remove('current_user');
+      $http.defaults.headers.common['X-User-Email'] = null;
+      $http.defaults.headers.common['X-User-Token'] = null;
+      $rootScope.current_user = null;
+    };
+
+    self.init_current_user = function(user) {
+      $cookieStore.put('current_user', JSON.stringify(user));
+      $rootScope.set_auth_headers(user.email, user.authentication_token);
+      $rootScope.current_user = user;
+    };
+
+    return self;
   });
